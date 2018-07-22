4d7947f941b74c90ead2ce35f6d25ef3816814d0
mattrudin <matthias.rudin@live.com>
2018-07-22 20:58:34 +0200

    Plan of action added

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-// complete the function
-function solution(string) {
-    
-}
+const breakCamelCase = (string) => {};
+
+
+// Plan of action
+// 1. identify the uppercase letter --> regex
+// 2. find the index of the uppercase letter --> UppercaseIndex = string.indexOf(regex);
+// 3. add an ' ' at the index before the uppercase letter --> string.splice(UppercaseIndex-1, 0, ' ');
